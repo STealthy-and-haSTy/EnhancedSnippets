77a7a5e0831134bb7e3fd2ff2a427af621ced1f6
Terence Martin <github@nurdz.com>
2023-02-13 21:59:44 -0800

    Let the insert_enhanced_snippet command to disable
    This change leverages the previous commit and enhances the command for
    inserting enhanced snippets so that it will enable and disable itself
    based on the current situation and either the information from the
    loaded snippet or the values provided in its arguments (for when the
    snippet is provided as textual content instead).

--- src/commands/insert_snippet.py
+++ src/commands/insert_snippet.py
@@ -12,32 +12,63 @@ class InsertEnhancedSnippetCommand(sublime_plugin.TextCommand):
     When invoked, this drops the currently cached version of all of the
     snippet enhancements and re-loads them.
     """
-    def run(self, edit, contents=None, name=None, **kwargs):
-        # We must get exactly one of the two arguments in order to proceed.
+    def run(self, edit, name=None, contents=None, scope='', glob='', **kwargs):
+        # We must get exactly one of the two source arguments in order to
+        # proceed.
         if not ((contents is None) ^ (name is None)):
             return log("insert_enhanced_snippet should be given exactly one of 'name' or 'contents'")
 
-        # Alias the manager for clarity
-        manager = SnippetManager.instance
+        # Try
+        snippet = self._get_snippet(name, contents, scope, glob)
+        if snippet is None:
+            return log(f"insert_enhanced_snippet was unable to find/load '{name}'")
 
+        # Expand it
+        snippet_args = snippet_expansion_args(snippet, SnippetManager.instance, kwargs)
+        self.view.run_command('insert_snippet', snippet_args)
+
+
+    def _get_snippet(self, name, contents, scope, glob):
+        """
+        Given either the name of a snippet file OR the contents, scope and
+        glob strings, return back a snippet instance that represents the
+        snippet in question.
+
+        The name provided can be an enhanced snippet or even a regular snippet
+        as desired.
+
+        If the arguments do not represent a valid snippet, returns None
+        """
         if contents is not None:
-            snippet = load_snippet(contents, is_resource=False)
+            return load_snippet(contents, scope, glob, is_resource=False)
         else:
             # Try to find the snippet as an enhanced snippet; the manager knows
             # about all of them
-            snippet =  manager.snippet_for_resource(name)
+            snippet =  SnippetManager.instance.snippet_for_resource(name)
             if snippet is None:
                 # If this is a normal snippet, try to directly load it so we
                 # can expand it.
                 if name.endswith('.sublime-snippet'):
                     snippet = load_snippet(name, is_resource=True)
 
-        if snippet is None:
-            return log(f"insert_enhanced_snippet was unable to find/load '{name}'")
+            return snippet
 
-        # Expand it
-        snippet_args = snippet_expansion_args(snippet, manager, kwargs)
-        self.view.run_command('insert_snippet', snippet_args)
+
+    def is_enabled(self, contents=None, name=None, scope='', glob='', **kwargs):
+        """
+        Only enable the command if the arguments represent a snippet that
+        applies in the current situation.
+
+        This will take either the provided scope and glob into account, or the
+        items from the value loaded from the named snippet, depending on the
+        arguments.
+        """
+        snippet = self._get_snippet(name, contents, scope, glob)
+        if snippet:
+            locations = [region.b for region in self.view.sel()]
+            return SnippetManager.instance.snippet_applies(snippet, self.view, locations)
+
+        return False
 
 
 ## ----------------------------------------------------------------------------
